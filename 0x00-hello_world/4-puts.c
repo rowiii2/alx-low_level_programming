f49027493faedfd979d90c1146c57d4a964b3dc4
rowiii2 <mostafarrm1822004@gmail.com>
2023-03-16 14:20:44 -0700

    sex commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - entry poit
-*return: always 0 (success)
+ *main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ *Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
